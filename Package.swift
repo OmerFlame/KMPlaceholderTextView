72db3ceac7840e6e44f5bc274b228bb06be78fc3
Omer Shamai <renginald.bagel@gmail.com>
2022-02-09 10:29:24 +0200

    Changed the name of the repo and targets

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "KMPlaceholderTextView",
+    name: "OSPlaceholderTextView",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "KMPlaceholderTextView",
-            targets: ["KMPlaceholderTextView"]),
+            name: "OSPlaceholderTextView",
+            targets: ["OSPlaceholderTextView"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "KMPlaceholderTextView",
+            name: "OSPlaceholderTextView",
             dependencies: []),
         .testTarget(
-            name: "KMPlaceholderTextViewTests",
-            dependencies: ["KMPlaceholderTextView"]),
+            name: "OSPlaceholderTextViewTests",
+            dependencies: ["OSPlaceholderTextView"]),
     ]
 )
